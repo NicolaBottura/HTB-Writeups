7a8398b80fdb313ef1aeb5f31d247d2f9ce5dc4a
Viilz1 <32301476+NicolaBottura@users.noreply.github.com>
2021-02-20 16:29:11 -0500

    Update EternalLoopCTF.py

--- EternalLoopCTF.py
+++ EternalLoopCTF.py
@@ -8,11 +8,10 @@ The password to unzip 6969.zip can be found using fcrackzip and the flag using S
 '''
 
 import zipfile
-import subprocess
 import os
 
 def extractor(filename):
-	zip_file = zipfile.ZipFile(filename, 'r')
+	zip_file = zipfile.ZipFile(filename)
 	file_info = zip_file.infolist()
 	filename = file_info[0].filename # filename the variable that contains the file name
 									 # of the zip in the infolist structure
@@ -22,17 +21,14 @@ def extractor(filename):
 	return filename
 
 def main():
-	path = "/home/kali/Downloads/"
 	first_zip = "37366.zip"
-	name = path + first_zip
+	name = first_zip
 	
 	while True:
 		name = extractor(name)
 		print(name)
 		if name == "6969.zip":
 			break
-		else:
-			os.remove(name)
 
 if __name__ == '__main__':
 	main()
