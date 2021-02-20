7654336d98499eaaa6280926d00bdad97261357c
Viilz1 <32301476+NicolaBottura@users.noreply.github.com>
2021-02-20 22:55:29 +0100

    Update deterministic.py

--- deterministic.py
+++ deterministic.py
@@ -1,3 +1,10 @@
+'''
+HTB/Challenges/Misc/Deterministic
+
+There is a locked door in front of us that can only be opened with the secret passphrase. 
+There are no keys anywhere in the room, only this .txt. There is also a writing on the wall.. "State 0: 69420, State N: 999, flag ends at state N, key 
+length: one".. Can you figure it out and open the door? 
+'''
 first_state = 69420
 last_state = 999
 
